a102f9eb7240adca65e8502c6b4889a28ebf8323
Elizaveta Vorobyeva <vorobeva.ed@it-o.ru>
2023-08-03 16:33:02 +0300

    fix

--- client/src/pages/user/MainUserPage.vue
+++ client/src/pages/user/MainUserPage.vue
@@ -1,27 +1,11 @@
 <script setup>
 import { MainUserLayout } from '../../layouts/index'
-import { BaseButton } from '../../shared/ui-kit/index.js'
 </script>
 
 <template>
   <main-user-layout>
     <div class="main-user-page">
-      <div class="main-user-page__container">
-        <base-button size="big">Смотреть все</base-button>
-        <base-button size="big" type="outlined">Смотреть все</base-button>
-        <base-button size="big" type="secondary">Кнопка</base-button>
-        <base-button size="big" type="light">
-          <template #icon-right> <fa-i icon="fa-solid fa-arrow-right" class="ml-3" /> </template>
-          <template #default> Кнопка </template>
-        </base-button>
-        <base-button size="middle">Кнопка</base-button>
-        <base-button size="middle" type="outlined">Кнопка</base-button>
-        <base-button size="middle" type="secondary">Кнопка</base-button>
-        <base-button size="middle" type="light">
-          <template #icon-left> <fa-i icon="fa-solid fa-bars" /> </template>
-          <template #default> Кнопка </template>
-        </base-button>
-      </div>
+      <div class="main-user-page__container"></div>
     </div>
   </main-user-layout>
 </template>
